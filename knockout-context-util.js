a00dcbafb14b9c1572632f9cb850f661f4dd891c
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-14 21:17:01 +0100

    Implement a create function that allows creating a customized module

--- knockout-context-util.js
+++ knockout-context-util.js
@@ -1,57 +1,77 @@
 define(['knockout', 'jquery'], function (ko, $) {
-  var getContext = function (elementAccessor) {
-    var context;
-    var element;
+  var defaultBodySelector = 'body';
 
-    if (!elementAccessor) {
-      return;
+  var createGetContext = function createGetContext () {
+    return function getContext (elementAccessor) {
+      var context;
+      var element;
+
+      if (!elementAccessor) {
+        return;
+      }
+
+      element = $(elementAccessor)[0];
+      if (!element) {
+        return;
+      }
+
+      context = ko.contextFor(element);
+      if (!context) {
+        return;
+      }
+
+      return context;
     }
-
-    element = $(elementAccessor)[0];
-    if (!element) {
-      return;
-    }
-
-    context = ko.contextFor(element);
-    if (!context) {
-      return;
-    }
-
-    return context;
   };
 
-  var getViewModel = function (elementAccessor) {
-    var viewModel;
-    var context = getContext(elementAccessor);
-    if (context) {
-      viewModel = context.$data;
-    } else {
-      viewModel = {};
-    }
-    return viewModel;
+  var createGetViewModel = function createGetViewModel () {
+    return function getViewModel (elementAccessor) {
+      var viewModel;
+      var context = getContext(elementAccessor);
+      if (context) {
+        viewModel = context.$data;
+      } else {
+        viewModel = {};
+      }
+      return viewModel;
+    };
   };
 
-  var getRoot = function () {
-    var context;
-    var root;
-
-    context = getContext('#globalBody');
-
-    if (!context) {
-      return;
+  var createGetRoot = function createGetRoot (bodySelector) {
+    if (typeof bodySelector !== 'string') {
+      bodySelector = defaultBodySelector;
     }
 
-    root = context.$root;
-    if (!root) {
-      return;
+    return function getRoot () {
+      var context;
+      var root;
+
+      context = getContext(bodySelector);
+
+      if (!context) {
+        return;
+      }
+
+      root = context.$root;
+      if (!root) {
+        return;
+      }
+
+      return root;
     }
-
-    return root;
   };
 
-  return {
-    getRoot: getRoot,
-    getContext: getContext,
-    getViewModel: getViewModel
+  var module = {
+    getContext: createGetContext(),
+    getRoot: createGetRoot(),
+    getViewModel: createGetViewModel()
+  }
+
+  module.create = function create (bodySelector) {
+    return $.extend({}, module, {
+      getRoot: createGetRoot(bodySelector)
+    });
   };
+
+  return module;
 });
